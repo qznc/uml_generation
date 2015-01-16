6bf9fd44275ed26acb3079570e3ed8ba844ff691
Andreas Zwinkau <zwinkau@kit.edu>
2015-01-16 12:37:07 +0100

    expand Lambda.java

--- Lambda.java
+++ Lambda.java
@@ -10,23 +10,36 @@ import java.util.Arrays;
  * @opt attributes
  * @opt types
  * @opt visibility
+ * @opt constructors
  * @hidden
  */
 class UMLOptions {}
 
 /**
  * @assoc - - - LambdaVisitor
+ * @composed - display - 0..1 VisibleEntity
+ * @opt nodefillcolor "#ddddff"
  */
-abstract class LambdaTerm {
-	abstract void accept(LambdaVisitor v);
+class LambdaTerm {
+	public abstract void accept(LambdaVisitor v);
+
+	public void registerObserver(LambdaTermObserver o) {}
+	public void unregisterObserver(LambdaTermObserver o) {}
+	public void notifyAppears() {}
+	public void notifyDisappears() {}
+	public void notifyColorChange() {}
 }
 
+/**
+ * @opt nodefillcolor "#ddddff"
+ */
 abstract class NamedLambdaTerm extends LambdaTerm {
 	String name;
 }
 
 /**
  * @composed - body - LambdaTerm
+ * @opt nodefillcolor "#ddddff"
  */
 class Abstraction extends NamedLambdaTerm {
 	public void accept(LambdaVisitor v) {}
@@ -35,31 +48,137 @@ class Abstraction extends NamedLambdaTerm {
 /**
  * @composed - left - LambdaTerm
  * @composed - right - LambdaTerm
+ * @opt nodefillcolor "#ddddff"
  */
 class Application extends LambdaTerm {
 	public void accept(LambdaVisitor v) {}
 }
 
+/**
+ * @opt nodefillcolor "#ddddff"
+ */
 class Variable extends NamedLambdaTerm {
 	public void accept(LambdaVisitor v) {}
 }
 
 /* VISITOR STUFF */
 
+/**
+ * @opt nodefillcolor "#ddddff"
+ */
 interface LambdaVisitor {
 	void visitAbstraction(Abstraction a);
 	void visitApplication(Application a);
 	void visitVariable(Variable v);
 }
 
+/**
+ * @opt nodefillcolor "#ddddff"
+ */
 class BetaReduction implements LambdaVisitor {
 	public void visitAbstraction(Abstraction a) {}
 	public void visitApplication(Application a) {}
 	public void visitVariable(Variable v) {}
 }
 
+/**
+ * @opt nodefillcolor "#ddddff"
+ */
 class AlphaConversion implements LambdaVisitor {
+	public AlphaConversion(String before, String after) {}
 	public void visitAbstraction(Abstraction a) {}
 	public void visitApplication(Application a) {}
 	public void visitVariable(Variable v) {}
+}
+
+/**
+ * Collects all abstraction and variable names within a lambda term.
+ * @opt nodefillcolor "#ddddff"
+ */
+class NameCollector implements LambdaVisitor {
+	public void visitAbstraction(Abstraction a) {}
+	public void visitApplication(Application a) {}
+	public void visitVariable(Variable v) {}
+}
+
+/**
+ * @assoc - - - LambdaTerm
+ * @opt nodefillcolor "#ddddff"
+ */
+interface LambdaTermObserver {
+	void appears(LambdaTerm t);
+	void disappears(LambdaTerm t);
+	void nameChange(LambdaTerm t);
+}
+
+/** VIEW **/
+
+/**
+ * @composed - entities * VisibleEntity
+ * @opt nodefillcolor "#ccffff"
+ */
+class AndroidView {
+	private List<VisibleEntity> entities;
+
+	public void appears(LambdaTerm t);
+	public void disappears(LambdaTerm t);
+	public void nameChange(LambdaTerm t);
+}
+
+/**
+ * @opt nodefillcolor "#ccffff"
+ */
+abstract class VisibleEntity implements LambdaTermObserver {
+}
+
+/**
+ * @opt nodefillcolor "#ccffff"
+ */
+class Alligator extends VisibleEntity { }
+
+/**
+ * @opt nodefillcolor "#ccffff"
+ */
+class Egg extends VisibleEntity { }
+
+/**
+ * @opt nodefillcolor "#ccffff"
+ */
+class OldAlligator extends VisibleEntity { }
+
+/** CONTROLLER **/
+
+/**
+ * @composed - - 1 EventSource
+ * @composed - displays 1 LambdaTerm
+ * @opt nodefillcolor "#ffffcc"
+ */
+class Controller {
+	public void appears(LambdaTerm t);
+	public void disappears(LambdaTerm t);
+	public void nameChange(LambdaTerm t);
+}
+
+/**
+ * @opt nodefillcolor "#ffffcc"
+ */
+abstract class EventSource {
+}
+
+/**
+ * @opt nodefillcolor "#ffffcc"
+ */
+class Keyboard extends EventSource {
+}
+
+/**
+ * @opt nodefillcolor "#ffffcc"
+ */
+class GameEvents extends EventSource implements LambdaTermObserver {
+}
+
+/**
+ * @opt nodefillcolor "#ffffcc"
+ */
+class TouchPad extends EventSource {
 }
